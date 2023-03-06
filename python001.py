a710747705b799babd0b706d1d140a9e8441b59f
woravit <86755992+woravi@users.noreply.github.com>
2023-03-06 10:20:48 +0700

    การบ้าน ep.1
    ส่งการบ้าน EP.1 โจทย์คือ: วาดรูปอะไรก็ได้ โดยห้ามมีเส้นขีดไปมา (ใช้คำสั่ง walk เหมือนที่สอนใน EP.1)

---
+++ python001.py
@@ -0,0 +1,49 @@
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def cir04():
+	
+for x in range(12):
+	tao.color('red')
+	tao.circle(30)
+	tao.right(30)
+tao.pu()
+tao.goto(x=180,y=0)
+tao.pd()
+
+for x in range(12):
+	tao.color('blue')
+	tao.circle(40)
+	tao.right(30)
+tao.pu()
+tao.goto(x=-180,y=0)
+tao.pd()
+for x in range(12):
+	tao.color('blue')
+	tao.circle(40)
+	tao.right(30)
+tao.pu()
+tao.goto(x=0,y=150)
+tao.pd()
+for x in range(12):
+	tao.color('orange')
+	tao.circle(40)
+	tao.right(30)
+tao.pu()
+tao.goto(x=-0,y=-150)
+tao.pd()
+for x in range(12):
+	tao.color('orange')
+	tao.circle(40)
+	tao.right(30)
+
+
+
+
+
+
+
+
+
+
+
